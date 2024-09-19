719e07db359dd46a62f0c45ec884b3bd7aa2b838
Tristan Reza Marchand <tristanmreza@gmail.com>
2024-09-19 11:59:46 +0200

    Fin TP1

---
+++ src/personnages/Romain.java
@@ -0,0 +1,43 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+		
+	@Override
+	public String toString() {
+		return "Romain [nom=" + nom + ", force=" + force + "]";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
+	
+	public static void main(String[] args) {
+		Romain cesar = new Romain("César", 6);
+		System.out.println(cesar);
+		cesar.recevoirCoup(4);
+	}
+}
