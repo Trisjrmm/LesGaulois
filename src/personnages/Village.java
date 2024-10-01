54bcf2c74d13cb9368601fc92f31757a9b6c0fc0
Tristan Reza Marchand <134953427+Trisjrmm@users.noreply.github.com>
2024-10-01 08:41:33 +0200

    Update village

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,5 +1,7 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 	private String nom;
 	private Chef chef;
@@ -19,7 +21,30 @@ public class Village {
 		return nom;
 	}
 	
-	public void ajouterHabitant(Gaulois gaulois, int index) {
-		System.out.println("TODO");
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	private Gaulois trouverHabitant(int i) {
+		return villageois[i];
+	}
+	
+	private void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois :");
+		for (int i = 0; i < nbVillageois; i++) {
+			System.out.println("- " + trouverHabitant(i).getNom());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des irrèductibles", 30);
+		Chef abracourcix = new Chef("Abracourcix", 6, village);
+		village.setChef(abracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 }
