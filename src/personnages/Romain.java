72808bad758cdd85ae31ef372e4f8f93eb90c7bb
Tristan Reza Marchand <134953427+Trisjrmm@users.noreply.github.com>
2024-10-01 09:46:20 +0200

    enum d'equipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipements = 0;
 	
 	public Romain(String nom, int force) {
 		assert force>0;
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 
 	public String getNom() {
@@ -22,10 +25,10 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 		
-//	@Override
-//	public String toString() {
-//		return "Romain [nom=" + nom + ", force=" + force + "]";
-//	}
+	@Override
+	public String toString() {
+		return "Romain [nom=" + nom + ", force=" + force + "]";
+	}
 
 	public void recevoirCoup(int forceCoup) {
 		assert force>0;
@@ -38,6 +41,21 @@ public class Romain {
 		}
 		assert force<forceOrig;
 	}
+	
+//	private void sEquiper(Equipement equipement) {
+//		switch (nbEquipements) {
+//			case 2:
+//				System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+//				break;
+//			case 1:
+//				if (equipements[nbEquipements-1] == equipement) {
+//					System.out.println("Le soldat " + nom + " possède déjà un " + equipement +" !");
+//				} else {
+//					equipements[nbEquipements] = equipement;
+//				}
+//		}
+//
+//	}
 	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
